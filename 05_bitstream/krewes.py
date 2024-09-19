226da6df14ff8b5933554054743b0228991b77ea
Andy Shyklo <ashyklo50@stuy.edu>
2024-09-19 11:02:28 -0400

    worked on random

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,3 +1,5 @@
+import random
+
 f = open("krewes.txt", "r")
 j = f.read()
 
@@ -14,16 +16,29 @@ for hx in g:
 
 print(h)
 
-dictP = {4: [], 5: []}
+dictP = {4: {}, 5: {}}
 
 for gx in h:
     print(gx[0])
     print(gx)
     if gx[0] == "4":
-        dictP[4].append({gx[1]: gx[2]})
+        dictP[4][gx[1]] = gx[2]
     elif gx[0] == "5":
-        dictP[5].append({gx[1], gx[2]})
+        dictP[5][gx[1]] = gx[2]
     else:
         print("h")
     
 print(dictP)
+
+a = random.randint(0, len(dictP.get(4)) + len(dictP.get(5)) - 1)
+print(a)
+print(f"Total amount of students: {len(dictP.get(4)) + len(dictP.get(5)) - 1}")
+print(f"Index: {a}")
+period = 0
+if (a<len(dictP.get(4))):
+    b = random.choice(dictP.get(4))
+    period = 4
+else:
+    b = random.choice(dictP.get(5))
+    period = 5
+print(f"Period: {period}, Name: {b}")
