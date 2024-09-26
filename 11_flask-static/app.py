f92e04fb2dce4b47b707488ead71f5d829aaff7a
AndyShyklo <andyshyklo@gmail.com>
2024-09-25 23:08:10 -0400

    finished CW11

--- 11_flask-static/app.py
+++ 11_flask-static/app.py
@@ -14,14 +14,20 @@ def hello_world():
     print(__name__)
     return "No hablo queso!"
 
-'''
 @app.route("/static/foo.html")
 def h():
     print("the __name__ of this module is... ")
     print(__name__)
     return str(random.random())
-'''
 
+'''
+@app.route("/static/fixie.html")
+def hi():
+    print("the __name__ of this module is... ")
+    print(__name__)
+    return "<h1>Team Name: Python Pigs</h1><br><h2>Roster: Ankita Saha, Andy Shyklo, Abidur Rahman</h2>"
+'''
+    
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
     app.run()
